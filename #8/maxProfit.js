f2a79e3f7aaa2da62c8b3db596c8ab54cd6ac0a8
cecicifu <ceciliocifuentes@gmail.com>
2021-12-17 02:12:06 +0100

    [Refactor] Code improvements

--- #8/maxProfit.js
+++ #8/maxProfit.js
@@ -3,18 +3,12 @@ export default function maxProfit(prices) {
     prices = prices.slice(
       prices.indexOf(Math.max(...prices)) + 1,
       prices.length
-    );
+    )
   if (Math.min(...prices) === prices[prices.length - 1])
-    prices = prices.slice(0, prices.indexOf(Math.min(...prices) - 1));
+    prices = prices.slice(0, prices.indexOf(Math.min(...prices) - 1))
 
-  const max = Math.max(...prices);
-  const min = Math.min(...prices);
+  const max = Math.max(...prices)
+  const min = Math.min(...prices)
 
-  for (let index = 0; index < prices.length; index++) {
-    if (max === min || prices[0] === max) {
-      return -1;
-    } else {
-      return max - min;
-    }
-  }
+  return (max === min || prices[0] === max) ? -1 : max - min
 }
